f8e050e45ad65f753dfc1e33ba5433c3675b878d
anshu <npn.anshu@gmail.com>
2023-06-27 18:55:21 +0545

    color generator working

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ for(let i = 0; i < gridSize; i++){
     grid.appendChild(cell);
   }
 }
+
+grid.addEventListener('mouseover', (event) => {
+  const cell = event.target;
+  const randomColor = getRandomColor();
+  cell.style.backgroundColor = randomColor;
+})
+
+function getRandomColor(){
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for(let i = 0; i < 6; i++){
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
