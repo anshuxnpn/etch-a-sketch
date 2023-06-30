6e2f55d2068b7d771448b5c9e2361aa0d006dafb
anshu <npn.anshu@gmail.com>
2023-06-30 11:39:11 +0545

    added the range input

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
 const gridContainer = document.getElementById('grid-container');
-const gridSize = 16;
+let gridSize = 16;
 
 const grid = document.createElement('div');
 grid.classList.add('grid');
 gridContainer.appendChild(grid);
-for(let i = 0; i < gridSize; i++){
-  for(let j = 0; j < gridSize; j++){
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    grid.appendChild(cell);
-  }
-}
-
 grid.addEventListener('mouseover', (event) => {
   const cell = event.target;
   const randomColor = getRandomColor();
   cell.style.backgroundColor = randomColor;
-})
+});
 
-function getRandomColor(){
+// Call createGrid() with default size
+createGrid(gridSize);
+
+function getRandomColor() {
   const letters = '0123456789ABCDEF';
   let color = '#';
-  for(let i = 0; i < 6; i++){
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
+}
+
+const createButton = document.getElementById('create-button');
+createButton.addEventListener('click', () => {
+  const gridSlider = document.getElementById('grid-slider');
+  const gridSize = gridSlider.value;
+  createGrid(gridSize);
+});
+
+function createGrid(gridSize) {
+  grid.innerHTML = '';
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      grid.appendChild(cell);
+    }
+  }
+  grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+}
+
+const clearButton = document.getElementById('clear-button');
+clearButton.addEventListener('click', clearGrid);
+
+function clearGrid() {
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach(cell => {
+    cell.style.backgroundColor = '';
+  });
 }
